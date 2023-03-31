edc4b2ff5b42252f796fb4c7b6963eecf56a1d38
mgracnazareno <mgracenazareno@gmail.com>
2023-03-31 12:04:39 -0400

    Fix login issues

--- Omnipop/src/omnipop/Omnipop.java
+++ Omnipop/src/omnipop/Omnipop.java
@@ -44,6 +44,7 @@ public class Omnipop {
         etudiant = new String[tEtudiant][6];
 
         System.out.println("Bienvenue au OMNIPOP");
+        boolean loginSuccess = false;
         while (infructueseTentative < MAX_TENTATIVES) {
             System.out.println("Entrez votre username: ");
             inputUsername = input.nextLine();
@@ -52,6 +53,7 @@ public class Omnipop {
 
             if (USERNAME.equals(inputUsername) && PASSWORD.equals(inputPassword)) {
                 System.out.println("Connexion reussi\n");
+                loginSuccess = true;
                 // clearScreen();
                 break;
             } else {
@@ -64,123 +66,125 @@ public class Omnipop {
                 }
             }
         }
-        do {
-            printMenu();
-            System.out.println("Entrez votre choix: (1 to 6)");
-            while (true) {
-                try {
-                    choice = Integer.parseInt(input.nextLine());
-                    if (choice >= 1 && choice <= 6) //break;
-                    {
-                        break;
+        if (loginSuccess) {
+            do {
+                printMenu();
+                System.out.println("Entrez votre choix: (1 to 6)");
+                while (true) {
+                    try {
+                        choice = Integer.parseInt(input.nextLine());
+                        if (choice >= 1 && choice <= 6) //break;
+                        {
+                            break;
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input, enter a number 1 to 6");
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input, enter a number 1 to 6");
                 }
-            }
-            //MENU PRINCIPAL
-            switch (choice) {
-                case 1:
-                    int courseListChoice = 0;
-                    getCourseListMenu();
-                    System.out.println("");
-                    System.out.println("Entrez votre choix: ");
-                    courseListChoice = validerEntree();
-                    //courseListChoice = input.nextInt();
-                    input.nextLine();
-                    System.out.println("");
-                    //MENU POUR LISTER TOUS LES COURS PAS SESSION
-                    switch (courseListChoice) {
-                        case 1:
-                            System.out.println("Cours pour l'Automne 2021");
-                            String courseFall = "";
-                            for (int row = 0; row < courseList.length; row++) {
-                                for (int col = 0; col < courseList[row].length; col++) {
-                                    courseFall = courseList[0][0] + "\t" + courseList[0][1] + "\t" + courseList[0][2];
+                //MENU PRINCIPAL
+                switch (choice) {
+                    case 1:
+                        int courseListChoice = 0;
+                        getCourseListMenu();
+                        System.out.println("");
+                        System.out.println("Entrez votre choix: ");
+                        courseListChoice = validerEntree();
+                        //courseListChoice = input.nextInt();
+                        input.nextLine();
+                        System.out.println("");
+                        //MENU POUR LISTER TOUS LES COURS PAS SESSION
+                        switch (courseListChoice) {
+                            case 1:
+                                System.out.println("Cours pour l'Automne 2021");
+                                String courseFall = "";
+                                for (int row = 0; row < courseList.length; row++) {
+                                    for (int col = 0; col < courseList[row].length; col++) {
+                                        courseFall = courseList[0][0] + "\t" + courseList[0][1] + "\t" + courseList[0][2];
+                                    }
                                 }
-                            }
-                            System.out.println(courseFall);
-                            System.out.println("");
-                            break;
-                        case 2:
-                            System.out.println("Cours pour d'Hiver 2021");
-                            String courseWinter = "";
-                            for (int row = 0; row < courseList.length; row++) {
-                                for (int col = 0; col < courseList[row].length; col++) {
-                                    courseWinter = courseList[1][0] + "\n" + courseList[1][1] + "\n" + courseList[1][2] + "\n" + courseList[1][3];
+                                System.out.println(courseFall);
+                                System.out.println("");
+                                break;
+                            case 2:
+                                System.out.println("Cours pour d'Hiver 2021");
+                                String courseWinter = "";
+                                for (int row = 0; row < courseList.length; row++) {
+                                    for (int col = 0; col < courseList[row].length; col++) {
+                                        courseWinter = courseList[1][0] + "\n" + courseList[1][1] + "\n" + courseList[1][2] + "\n" + courseList[1][3];
+                                    }
                                 }
-                            }
-                            System.out.println(courseWinter);
-                            System.out.println("");
-                            break;
-                        case 3:
-                            System.out.println("Cours pour l'Été 2022");
-                            String courseSummer = "";
-                            for (int row = 0; row < courseList.length; row++) {
-                                for (int col = 0; col < courseList[row].length; col++) {
-                                    courseSummer = courseList[2][0] + "\n" + courseList[2][1] + "\n" + courseList[2][2];
+                                System.out.println(courseWinter);
+                                System.out.println("");
+                                break;
+                            case 3:
+                                System.out.println("Cours pour l'Été 2022");
+                                String courseSummer = "";
+                                for (int row = 0; row < courseList.length; row++) {
+                                    for (int col = 0; col < courseList[row].length; col++) {
+                                        courseSummer = courseList[2][0] + "\n" + courseList[2][1] + "\n" + courseList[2][2];
+                                    }
                                 }
-                            }
-                            System.out.println(courseSummer);
-                            System.out.println("");
-                            break;
-                        default:
-                            System.out.println("Éntrée est invalide, entrez une numeric valeur de 1 à 4");
-                            break;
-                    }
-                    break;
+                                System.out.println(courseSummer);
+                                System.out.println("");
+                                break;
+                            default:
+                                System.out.println("Éntrée est invalide, entrez une numeric valeur de 1 à 4");
+                                break;
+                        }
+                        break;
 
-                case 2:
-                    System.out.println("List of all courses\n");
-                    displayCourses(courseList);
-                    break;
-                case 3:
-                    System.out.println("Combien d'étudiants voulez-vous ajouter dans le dossier ?: ");
-                    tEtudiant = input.nextInt();
-                    //input.nextLine();
-                    etudiant = new String[tEtudiant][6];
-                    getStudentInfo(etudiant);
+                    case 2:
+                        System.out.println("List of all courses\n");
+                        displayCourses(courseList);
+                        break;
+                    case 3:
+                        System.out.println("Combien d'étudiants voulez-vous ajouter dans le dossier ?: ");
+                        tEtudiant = input.nextInt();
+                        //input.nextLine();
+                        etudiant = new String[tEtudiant][6];
+                        getStudentInfo(etudiant);
 
-                    break;
-                case 4:
+                        break;
+                    case 4:
 
-                    System.out.println("Lister tous les étudiants");
-                    if (tEtudiant > 0) {
-                        displayStudents(etudiant);
-                    } else {
-                        System.out.println("Il n'y a pas d'étudiant dans la liste");
+                        System.out.println("Lister tous les étudiants");
+                        if (tEtudiant > 0) {
+                            displayStudents(etudiant);
+                        } else {
+                            System.out.println("Il n'y a pas d'étudiant dans la liste");
 
-                    }
-                    break;
+                        }
+                        break;
 
-                case 5:
-                    System.out.println("Rechercher un étudiant, veuillez entrez le nom et prenom");
-                    System.out.print("Enter le nom: ");
-                    nom = input.nextLine();
-                    System.out.print("Entrez le prenom: ");
-                    prenom = input.nextLine();
-                    int studentIndex = findStudentIndex(etudiant, nom, prenom);
-                    if (studentIndex == -1) {
-                        System.out.println("Aucun étudiant trouvé avec ce nom et prénom.");
-                    } else {
-                        System.out.printf("%-4s%-15s%-15s%-20s%-25s%-15s\n", "Id", "Nom", "Prenom", "Date de naissance", "Email", "Code Permanent");
-                        System.out.printf("%-4s%-15s%-15s%-20s%-25s%-15s\n", etudiant[studentIndex][0], etudiant[studentIndex][1], etudiant[studentIndex][2], etudiant[studentIndex][3], etudiant[studentIndex][4], etudiant[studentIndex][5]);
-             
-                    }
-                    break;
-                    
-                case 6:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Entree est invalide!");
-                    break;
-            }
-            // Prompt user to continue or exit
-            System.out.println("Veuillez vous faire une autre tache? (oui ou non): ");
-            response = input.next();
-            input.nextLine();
-        } while (response.equalsIgnoreCase("oui"));
+                    case 5:
+                        System.out.println("Rechercher un étudiant, veuillez entrez le nom et prenom");
+                        System.out.print("Enter le nom: ");
+                        nom = input.nextLine();
+                        System.out.print("Entrez le prenom: ");
+                        prenom = input.nextLine();
+                        int studentIndex = findStudentIndex(etudiant, nom, prenom);
+                        if (studentIndex == -1) {
+                            System.out.println("Aucun étudiant trouvé avec ce nom et prénom.");
+                        } else {
+                            System.out.printf("%-4s%-15s%-15s%-20s%-25s%-15s\n", "Id", "Nom", "Prenom", "Date de naissance", "Email", "Code Permanent");
+                            System.out.printf("%-4s%-15s%-15s%-20s%-25s%-15s\n", etudiant[studentIndex][0], etudiant[studentIndex][1], etudiant[studentIndex][2], etudiant[studentIndex][3], etudiant[studentIndex][4], etudiant[studentIndex][5]);
+
+                        }
+                        break;
+
+                    case 6:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Entree est invalide!");
+                        break;
+                }
+                // Prompt user to continue or exit
+                System.out.println("Veuillez vous faire une autre tache? (oui ou non): ");
+                response = input.next();
+                input.nextLine();
+            } while (response.equalsIgnoreCase("oui"));
+        }
         input.close();
     }
 
