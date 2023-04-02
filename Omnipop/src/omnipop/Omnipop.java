e8259c7e17cadfd031dacba70bb9e0e98507674b
mgracnazareno <mgracenazareno@gmail.com>
2023-04-02 01:21:14 -0400

    Ajoute les validations

--- Omnipop/src/omnipop/Omnipop.java
+++ Omnipop/src/omnipop/Omnipop.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -83,9 +84,21 @@ public class Omnipop {
                             getCourseListMenu();
                             System.out.println("");
                             System.out.println("Entrez votre choix: ");
-                            courseListChoice = validerEntree();
-                            //input.nextLine();
-                            //System.out.println("");
+                            while(true){
+                                if (input.hasNextInt()) {
+                                    courseListChoice = input.nextInt();
+                                    if (courseListChoice >= 1 && courseListChoice <= 3) {
+                                        break;
+                                    }else{
+                                        System.out.println("Entrez une valeur numerique entre 1 et 3!");
+                                    }
+                                    
+                                }else{
+                                    System.out.println("Entrez une valeur numerique seulement");
+                                    input.next();
+                                }
+                            }
+                           input.nextLine();
                             //MENU POUR LISTER TOUS LES COURS PAS SESSION
                             switch (courseListChoice) {
                                 case 1:
@@ -133,7 +146,7 @@ public class Omnipop {
                             break;
                         case 3:
                             System.out.println("Combien d'étudiants voulez-vous ajouter dans le dossier ?: ");
-                            tEtudiant = input.nextInt();
+                            tEtudiant = validerEntree();
                             //input.nextLine();
                             etudiant = new String[tEtudiant][6];
                             getStudentInfo(etudiant);
@@ -225,20 +238,36 @@ public class Omnipop {
             System.out.println("Entrez l'information d'Etudiant " + id++);
 
             System.out.print("Nom de famille: ");
-            String nom = input.nextLine();           
+            String nom = "";
+            while (nom.isEmpty() || !nom.matches("^[a-zA-Z]+$")) {
+
+                if (!nom.matches("^[a-zA-Z]+$")) {
+                    System.out.println("Le nom doit contenir seulement des lettres. Veuillez réessayer\n"
+                            + "Entrez le nom: ");
+                }
+            }
 
             System.out.print("Prenom: ");
-            String prenom = input.nextLine();
-             
-             
+            String prenom = "";
+            while (nom.isEmpty() || !nom.matches("^[a-zA-Z]+$")) {
+                prenom = input.nextLine().trim();
+                if (!nom.matches("^[a-zA-Z]+$")) {
+                    System.out.println("Le prenom doit contenir seulement des lettres. Veuillez réessayer\n"
+                            + "Entrez le prenom: ");
+
+                }
+            }
+
             System.out.print("Année de naissance: ");
-            String aNaissance = input.nextLine();
-            
-             
+            String aNaissance=input.nextLine();
+           
             System.out.print("Email: ");
             String email = input.nextLine();
-             
-
+            String regexEmail ="^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+            if (!email.matches(regexEmail)) {
+                System.out.println("Le courriel est invaliee");
+                return;
+            }
             try {
                 String codeP = genererCodePermanent(nom, prenom, aNaissance);
                 etudiant[row][0] = String.valueOf(row + 1);
@@ -292,58 +321,6 @@ public class Omnipop {
             return false;
         }
     }
-//
-//    private static boolean validerNom(String nom) {
-//        Pattern pattern = Pattern.compile("^[a-zA-ZÀ-ÖØ-öø-ÿ]*([ '-][a-zA-ZÀ-ÖØ-öø-ÿ]*)*$");
-//        Matcher matcher = pattern.matcher(nom);
-//        if (!matcher.matches()) {
-//            System.out.println("Le nom est invalide, Le nom doit commencer par une majuscule"
-//                    + "suive de letters miniscules et éventuellement tiret suivi d'une majuscule et "
-//                    + "de lettres minuscules");
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    private static boolean validerPrenom(String prenom) {
-//        Pattern pattern = Pattern.compile("^[A-Z][a-z]*$");
-//        Matcher matcher = pattern.matcher(prenom);
-//        if (!matcher.matches()) {
-//            System.out.println("Le prénom est invalide. Le prénom doit commencer par une majuscule suivie de lettres minuscules.");
-//            return false;
-//        }
-//        return true;
-//    }
-//    private static boolean validerAnnee(String year){
-//        Pattern pattern = Pattern.compile("^[0-9]{4}$");
-//        Matcher matcher = pattern.matcher(year);
-//        
-//        return matcher.matches();
-//    }
-//
-//    private static boolean validerAnnee(String annee) {
-//        try {
-//            int year = Integer.parseInt(annee);
-//            if (annee.length() != 4) {
-//                System.out.println("Attention: L'année doit être de quatre chiffres");
-//                return false;
-//            }
-//            return true;
-//        } catch (NumberFormatException e) {
-//            System.out.println("Attention: L'année doit être un nombre");
-//            return false;
-//        }
-//    }
-//
-//    private static boolean validerCourriel(String courriel) {
-//        Pattern pattern = Pattern.compile("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
-//        Matcher matcher = pattern.matcher(courriel);
-//        if (!matcher.matches()) {
-//            System.out.println("Le courriel est invalide. Le courriel doit avoir un format valide, par exemple: exemple@domaine.com.");
-//            return false;
-//        }
-//        return true;
-//    }
 
     private static int findStudentIndex(String[][] students, String nom, String prenom) {
         for (int i = 0; i < students.length; i++) {
